73a0f086016c373914cbcfd52f4ec0e377d4b264
shawnw <shawnw@speakeasy.org>
2016-01-08 00:40:05 -0800

    Improve op2bin.pl

--- op2bin.pl
+++ op2bin.pl
@@ -3,8 +3,11 @@ use warnings;
 use strict;
 
 # Read a comma-seperated list of numbers and produce a synacor challenge
-# binary file from them.
+# binary file from them. Used with the example in arch-spec.
+# usage:
+# perl op2bin.pl < test.op > test.bin
 
+binmode STDOUT;
 while (<>) {
 	my @nums = split /,/;
 	print pack("v*", @nums);
