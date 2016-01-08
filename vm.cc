e9effae4711d59e72e6d00cf58d532d647a89d83
shawnw <shawnw@speakeasy.org>
2016-01-08 00:07:39 -0800

    Some minor optimizations.

--- vm.cc
+++ vm.cc
@@ -5,15 +5,28 @@
 #include <stack>
 #include <vector>
 #include <stdexcept>
-#include <cstring>
 #include <fstream>
 #include <string>
 #include <functional>
 #include <cstdlib>
 
-// Number related stuff.
+// Control what type is used to represent words. Fastest, or exact.
+#define FAST_WORD
+// Disables some bounds and consistency checks. Should be okay on
+// well formed binary images.
+#define UNSAFE
 
+#ifdef FAST_WORD
+using numtype = std::uint_fast16_t;
+#else
 using numtype = std::uint16_t;
+#endif
+
+#ifdef UNSAFE
+#define AT(c, i) c[i]
+#else
+#define AT(c, i) c.at(i)
+#endif
 
 constexpr numtype fix15(numtype a) {
 	return a & 0b0111111111111111;
@@ -47,40 +60,39 @@ private:
 	numtype pc{0};
 	registers regs{{0,0,0,0,0,0,0,0}};
 	stack s;
-	numtype a{0},b{0},c{0};
 
 	numtype val(numtype);
 	numtype load(numtype);
 	void store(numtype, numtype);
-	void writeval(numtype, numtype);
+	void regstore(numtype, numtype);
 
-#define A   pc += 1; a = mem.at(pc++)
-#define AB  A; b = mem.at(pc++)
-#define ABC AB; c = mem.at(pc++)	
+#define A   pc += 1; numtype a{AT(mem, pc++)}
+#define AB  A; numtype b{AT(mem, pc++)}
+#define ABC AB; numtype c{AT(mem, pc++)} 
 	std::array<std::function<void(void)>, 22>
 	ops{{
 		[](){ std::exit(0); }, // 0 halt
-		[&](){ AB; writeval(a, val(b)); }, // 1 set
+		[&](){ AB; regstore(a, val(b)); }, // 1 set
 		[&](){ A; s.push(val(a)); }, // 2 push
 		[&](){ A; if (s.empty()) throw std::runtime_error{"empty stack"};
-					 writeval(a, s.top()); s.pop(); }, // 3 pop
-		[&](){ ABC; writeval(a, val(b) == val(c)); }, // 4 eq
-		[&](){ ABC; writeval(a, val(b) > val(c)); }, // 5 gt
+					 regstore(a, s.top()); s.pop(); }, // 3 pop
+		[&](){ ABC; regstore(a, val(b) == val(c)); }, // 4 eq
+		[&](){ ABC; regstore(a, val(b) > val(c)); }, // 5 gt
 		[&](){ A; pc = val(a); }, // 6 jmp
 		[&](){ AB; if (val(a) != 0) pc = val(b); }, // 7 jt
 		[&](){ AB; if (val(a) == 0) pc = val(b); }, // 8 jf
-		[&](){ ABC; writeval(a, (val(b) + val(c)) % M); }, // 9 add
-		[&](){ ABC; writeval(a, (val(b) * val(c)) % M); }, // 10 mult
-		[&](){ ABC; writeval(a, val(b) % val(c)); }, // 11 mod
-		[&](){ ABC; writeval(a, fix15(val(b) & val(c))); }, // 12 and
-		[&](){ ABC; writeval(a, fix15(val(b) | val(c))); }, // 13 or
-		[&](){ AB; writeval(a, fix15(~val(b))); }, // 14 not
-		[&](){ AB; writeval(a, load(val(b))); }, // 15 rmem
+		[&](){ ABC; regstore(a, (val(b) + val(c)) % M); }, // 9 add
+		[&](){ ABC; regstore(a, (val(b) * val(c)) % M); }, // 10 mult
+		[&](){ ABC; regstore(a, val(b) % val(c)); }, // 11 mod
+		[&](){ ABC; regstore(a, val(b) & val(c)); }, // 12 and
+		[&](){ ABC; regstore(a, val(b) | val(c)); }, // 13 or
+		[&](){ AB; regstore(a, fix15(~val(b))); }, // 14 not
+		[&](){ AB; regstore(a, load(val(b))); }, // 15 rmem
 		[&](){ AB; store(val(a), val(b)); }, // 16 wmem
 		[&](){ A; s.push(pc); pc = val(a); }, // 17 call
 		[&](){ if (s.empty()) std::exit(0); pc = s.top(); s.pop(); }, // 18 ret
 		[&](){ A; std::cout << static_cast<char>(val(a)); }, // 19 out
-		[&](){ A; writeval(a, std::cin.get()); }, // 20 in
+		[&](){ A; regstore(a, std::cin.get()); }, // 20 in
 		[&](){ pc += 1; } // 21 noop
 	}};
 
@@ -96,7 +108,8 @@ private:
 image::image(std::istream &in) {
 	std::cout << "Reading program..." << std::flush; 
 	 do {
-	 	 char raw[sizeof(numtype) * 50];
+	 	 // Read 1k words at a time.
+	 	 char raw[sizeof(std::uint16_t) * 1024];
 	 	 in.read(raw, sizeof raw);
 	 	 if (in.gcount() > 0) {
 	 	 	 if (in.gcount() % 2 != 0) {
@@ -117,12 +130,19 @@ image::image(std::istream &in) {
 // Convert a numtype to a number, or return the value
 // in a register if numtype refers to one.
 numtype image::val(numtype n) {
+#ifdef UNSAFE
+	if (is_number(n))
+		return n;
+	else
+		return regs[to_register(n)];
+#else
 	if (is_number(n))
 		return n;
 	else if (is_register(n))
 		return regs[to_register(n)];
 	else
 		throw std::runtime_error{"Invalid number."};
+#endif
 }
 
 // Return the value stored in a memory location
@@ -140,17 +160,21 @@ void image::store(numtype addr, numtype v) {
 }
 
 // Write val to the register a
-void image::writeval(numtype r, numtype val) {
+void image::regstore(numtype r, numtype val) {
+#ifdef UNSAFE
+	regs[to_register(r)] = val;
+#else
 	if (is_register(r))
 		regs[to_register(r)] = val;
 	else 
 		throw std::runtime_error("not a register");
+#endif
 }
 
 void image::run(void) {
 	while (pc < mem.size()) {
-		auto insn = mem.at(pc);
-		ops.at(insn)();
+		auto insn = mem[pc];
+		AT(ops, insn)();
 	}
 }
 
