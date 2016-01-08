97a44a4618df52520aee18ba1b057270bc489b3e
shawnw <shawnw@speakeasy.org>
2016-01-08 14:06:58 -0800

    Improved endianness conversion in image loading code. More comments. Minor reorganization and removal of unused functions. Ending running a program doesn't make the VM automatically exit.

--- vm.cc
+++ vm.cc
@@ -1,6 +1,18 @@
 /* Virtual Machine for Synacor Challenge, take 1 */
+
+/* Normally, when I write an interpeter for a small bytecode language, I
+ * use a big switch statement to dispatch, one case per opcode. This time
+ * I decided to try something slightly different, though, and use an array
+ * of functions, with index corresponding to the opcode. C++11 lambdas make
+ * defining this array all happen in one spot, and ends up visually looking
+ * a lot like the classic switch. The actual execution of the program turns
+ * into two lines of while loop and function call.
+ *
+ * Possible other directions, for fun and profit: threaded code turning it
+ * into an array of functions, and JIT compiling a program to native code.
+ */
+
 #include <iostream>
-#include <cstdint>
 #include <array>
 #include <stack>
 #include <vector>
@@ -8,12 +20,17 @@
 #include <fstream>
 #include <string>
 #include <functional>
-#include <cstdlib>
+#include <cstdint>
 
-// Control what type is used to represent words. Fastest, or exact.
+#include <boost/endian/conversion.hpp>
+
+/* Control what type is used to represent words. Fastest for the host system,
+   or exactly 16 bits.
+*/
 #define FAST_WORD
-// Disables some bounds and consistency checks. Should be okay on
-// well formed binary images.
+/* Disables some bounds and consistency checks. Should be okay on
+   well formed binary images.
+*/
 #define UNSAFE
 
 #ifdef FAST_WORD
@@ -28,11 +45,11 @@ using numtype = std::uint16_t;
 #define AT(c, i) c.at(i)
 #endif
 
+constexpr numtype M{32768}; 
+
 constexpr numtype fix15(numtype a) {
 	return a & 0b0111111111111111;
 }
-
-constexpr int M{32768}; 
 
 constexpr bool is_number(numtype n) {
 	return n < 32768;
@@ -44,10 +61,6 @@ constexpr bool is_register(numtype n) {
 	
 constexpr int to_register(numtype n) {
 	return n - 32768;
-}
-
-constexpr bool is_valid(numtype n) {
-	return n < 32776;
 }
 
 class image {
@@ -66,12 +79,14 @@ private:
 	void store(numtype, numtype);
 	void regstore(numtype, numtype);
 
+	struct end_of_program{};
+
 #define A   pc += 1; numtype a{AT(mem, pc++)}
 #define AB  A; numtype b{AT(mem, pc++)}
 #define ABC AB; numtype c{AT(mem, pc++)} 
 	std::array<std::function<void(void)>, 22>
 	ops{{
-		[](){ std::exit(0); }, // 0 halt
+		[](){  throw end_of_program(); }, // 0 halt
 		[&](){ AB; regstore(a, val(b)); }, // 1 set
 		[&](){ A; s.push(val(a)); }, // 2 push
 		[&](){ A; if (s.empty()) throw std::runtime_error{"empty stack"};
@@ -90,8 +105,8 @@ private:
 		[&](){ AB; regstore(a, load(val(b))); }, // 15 rmem
 		[&](){ AB; store(val(a), val(b)); }, // 16 wmem
 		[&](){ A; s.push(pc); pc = val(a); }, // 17 call
-		[&](){ if (s.empty()) std::exit(0); pc = s.top(); s.pop(); }, // 18 ret
-		[&](){ A; std::cout << static_cast<char>(val(a)); }, // 19 out
+		[&](){ if (s.empty()) throw end_of_program(); pc = s.top(); s.pop(); }, // 18 ret
+		[&](){ A; std::cout.put(static_cast<char>(val(a))); }, // 19 out
 		[&](){ A; regstore(a, std::cin.get()); }, // 20 in
 		[&](){ pc += 1; } // 21 noop
 	}};
@@ -101,31 +116,9 @@ private:
 #undef A
 	
 	public:	
-		explicit image(std::istream &in);
+		explicit image(std::istream &);
 		void run(void);		
 };
-
-image::image(std::istream &in) {
-	std::cout << "Reading program..." << std::flush; 
-	 do {
-	 	 // Read 1k words at a time.
-	 	 char raw[sizeof(std::uint16_t) * 1024];
-	 	 in.read(raw, sizeof raw);
-	 	 if (in.gcount() > 0) {
-	 	 	 if (in.gcount() % 2 != 0) {
-	 	 	 	 // Couldn't read a full 16 bit word 
-	 	 	 	 throw std::runtime_error{"Unable to read full word from input file."};
-	 	 	 }
-	 	 	 mem.reserve(mem.size() + (in.gcount()/2));
-	 	 	 for (int n = 0; n < in.gcount(); n += 2) {
-	 	 	 	 numtype word = static_cast<unsigned char>(raw[n])
-	 	 	 	 	+ (static_cast<unsigned char>(raw[n+1]) << 8);
-	 	 	 	 mem.push_back(word);
-	 	 	 }
-	 	 }
-	 } while (in.good());
-	 std::cout << " done. Read " << mem.size() << " words.\n";
-}
 
 // Convert a numtype to a number, or return the value
 // in a register if numtype refers to one.
@@ -147,19 +140,29 @@ numtype image::val(numtype n) {
 
 // Return the value stored in a memory location
 numtype image::load(numtype addr) {
+#ifndef UNSAFE
+	if (!is_number(addr))
+		throw std::runtime_error("Trying to laod from an invalid address.");
+#endif
 	if (mem.size() <= addr)
 		return 0;
 	else
 		return mem[addr];
 }
 
+// Write a value to a memory location.
 void image::store(numtype addr, numtype v) {
+#ifndef UNSAFE
+	if (!is_number(addr))
+		throw std::runtime_error("Trying to store in an invalid address.");
+#endif
 	if (mem.size() <= addr)
 		mem.resize(addr + 1);
+
 	mem[addr] = v;
 }
 
-// Write val to the register a
+// Write val to the encoded register r
 void image::regstore(numtype r, numtype val) {
 #ifdef UNSAFE
 	regs[to_register(r)] = val;
@@ -171,10 +174,35 @@ void image::regstore(numtype r, numtype val) {
 #endif
 }
 
+image::image(std::istream &in) {
+	std::cout << "Reading program..." << std::flush; 
+	 do {
+	 	 // Read 1k words at a time.
+	 	 char raw[sizeof(std::uint16_t) * 1024];
+	 	 in.read(raw, sizeof raw);
+	 	 auto bytes = in.gcount();
+	 	 if (bytes > 0) {
+	 	 	 if (bytes % 2 != 0) {
+	 	 	 	 // Couldn't read a full 16 bit word 
+	 	 	 	 throw std::runtime_error{"Unable to read full word from input file."};
+	 	 	 }
+	 	 	 mem.reserve(mem.size() + (bytes/2));
+	 	 	 for (int n = 0; n < bytes; n += 2) {
+	 	 	 	 std::uint16_t *word = reinterpret_cast<std::uint16_t*>(raw + n);
+	 	 	 	 mem.push_back(boost::endian::little_to_native(*word));
+	 	 	 }
+	 	 }
+	 } while (in.good());
+	 std::cout << " done. Read " << mem.size() << " words.\n";
+}
+
 void image::run(void) {
-	while (pc < mem.size()) {
-		auto insn = mem[pc];
-		AT(ops, insn)();
+	try {
+		while (pc < mem.size()) {
+			AT(ops, mem[pc])();
+		}
+	} catch (end_of_program) {
+		return;
 	}
 }
 
